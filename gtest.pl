d35bd87e08ca2d2e6a2f419a6785941d9844909a
drags <github@timsogard.com>
2009-09-23 02:22:22 -0400

    Moar rrd work

--- gtest.pl
+++ gtest.pl
@@ -3,6 +3,11 @@ use Warsow::Poller;
 
 my $poller = new Poller;
 
-$poller->updateGraphs('so.nuclearfallout.net','44400');
+$poller->updateGraphs('216.52.143.228','44400');
+$poller->updateGraphs('66.150.214.231','44400');
+$poller->updateGraphs('75.102.38.26','44400');
+$poller->updateGraphs('74.86.100.40','44451');
+$poller->updateGraphs('74.86.100.40','44453');
 $poller->updateGraphs('78.46.73.70','44400');
 $poller->updateGraphs('89.248.165.13','44400');
+$poller->updateGraphs('so.nuclearfallout.net','44400');
